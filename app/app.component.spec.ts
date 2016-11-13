390f304fc5f69899c8fce85e5ee0000032872a1c
Axel Hodler <derxor@gmail.com>
2016-11-13 10:14:57 +0100

    rm unused functionality

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -2,10 +2,8 @@
 import { AppComponent } from './app.component';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-////////  SPECS  /////////////
 describe('AppComponent', function () {
   let de: DebugElement;
   let comp: AppComponent;
@@ -21,15 +19,8 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
-  it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
-  });
 });
