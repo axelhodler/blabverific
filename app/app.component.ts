9ebed70167243120b7e6939a552502c998ee0867
Axel Hodler <derxor@gmail.com>
2016-11-19 12:26:22 +0100

    improve ui

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'blab-verification',
-  template: '<submit-report></submit-report><verify-report></verify-report>'
+  template: '<submit-report></submit-report><br><br><div><verify-report></verify-report></div>'
 })
 export class AppComponent {
 }
