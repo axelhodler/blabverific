871009677db4ee106e85c05b18d64723b62565ef
Axel Hodler <derxor@gmail.com>
2016-11-20 18:57:28 +0100

    only displays submit button if report entered

--- app/submitreport.component.spec.ts
+++ app/submitreport.component.spec.ts
@@ -6,6 +6,7 @@ import {
 import {FormsModule} from "@angular/forms";
 import {SubmitReportPageObject} from "./submitreport.component.pageobject";
 import {Contract} from "./boundaries/contract";
+import {By} from "@angular/platform-browser";
 
 describe('SubmitReportComponent', function () {
   let pageObject: SubmitReportPageObject;
@@ -40,11 +41,21 @@ describe('SubmitReportComponent', function () {
     expect(pageObject.hashedReport()).toBe('0x5917c10d9344319535b34bb5b24b1df303f6fdd691c74ea5f0f66cb1f19f07af');
   });
 
+  it('does not display submit button as enabled if nothing is entered', () => {
+    expect(fixture.debugElement.query(By.css('#submit-report')).nativeElement.getAttribute('disabled')).toBe('');
+  });
+
+  it('displays submit button as enabled if a report was entered', () => {
+    pageObject.insertReportContent('my report');
+
+    expect(fixture.debugElement.query(By.css('#submit-report')).nativeElement.getAttribute('disabled')).toBeNull();
+  });
+
   it('submits the hash of the entered report', () => {
     pageObject.insertReportContent('my report');
 
     pageObject.clickSubmitReport();
 
     expect(contractSpy.submitReport).toHaveBeenCalledWith('0xd2a1ba85429ae235e1572871497ae0d0e499c696cb44d33f88c2a26820e4f7cc');
-  })
+  });
 });
