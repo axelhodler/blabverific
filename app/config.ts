7f22ac0cfe37b5aa0865ecf600f0c0971d98b50f
Axel Hodler <derxor@gmail.com>
2016-11-23 20:17:18 +0100

    provider is set by metamask

--- app/config.ts
+++ app/config.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Config {
-  readonly CONTRACT_PROVIDER: string = '_CONTRACT_PROVIDER_';
   readonly CONTRACT_ADDRESS: string = '_CONTRACT_ADDRESS_';
   readonly CONTRACT_ABI: string = '_CONTRACT_ABI_';
 }
--- bin/run.sh
+++ bin/run.sh
@@ -5,13 +5,10 @@ CONFIG_CONTENTS=`./bin/prepare_contract.sh`
 CONFIG_CONTENTS_ARRAY=($CONFIG_CONTENTS)
 cd ..
 
-PROVIDER=${CONFIG_CONTENTS_ARRAY[0]}
-sed -i '' 's#_CONTRACT_PROVIDER_#'$PROVIDER'#g' app/config.ts
-
-ADDRESS=${CONFIG_CONTENTS_ARRAY[1]}
+ADDRESS=${CONFIG_CONTENTS_ARRAY[0]}
 sed -i '' 's#_CONTRACT_ADDRESS_#'$ADDRESS'#g' app/config.ts
 
-ABI=${CONFIG_CONTENTS_ARRAY[2]}
+ABI=${CONFIG_CONTENTS_ARRAY[1]}
 sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' app/config.ts
 
 trap "lsof -t -i:8545 | xargs kill && git checkout app/config.ts" SIGINT
