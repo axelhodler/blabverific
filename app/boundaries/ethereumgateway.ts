facab5dbe506e6815d23a1ec7bb2ff606afcbd28
Axel Hodler <derxor@gmail.com>
2016-11-23 20:14:54 +0100

    metamask provides this

--- app/boundaries/ethereumgateway.ts
+++ app/boundaries/ethereumgateway.ts
@@ -1,21 +1,18 @@
 import {Injectable} from "@angular/core";
 import {Config} from "../config";
-declare var Web3: any;
+declare var web3: any;
 
 @Injectable()
 export class EthereumGateway {
-  web3: any;
   contract: any;
 
   constructor(private config: Config) {}
 
   connectToContract() {
-    this.web3 = new Web3();
-    this.web3.setProvider(new this.web3.providers.HttpProvider(this.config.CONTRACT_PROVIDER));
-    this.contract = this.web3.eth.contract(JSON.parse(this.config.CONTRACT_ABI)).at(this.config.CONTRACT_ADDRESS);
+    this.contract = web3.eth.contract(JSON.parse(this.config.CONTRACT_ABI)).at(this.config.CONTRACT_ADDRESS);
   }
 
   currentUserAddress() {
-    return this.web3.eth.coinbase;
+    return web3.eth.coinbase;
   }
 }
