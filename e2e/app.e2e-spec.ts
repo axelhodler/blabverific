86043009757a20e0f26f8c97fca9d985b55468dd
Axel Hodler <derxor@gmail.com>
2017-01-03 20:48:08 +0100

    no need for the beforeEach

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,11 +4,9 @@ describe('Blabverific E2E Tests', () => {
 
   let expectedMsg = 'blockLAB Verify';
 
-  beforeEach(() => {
+  it('should display: ' + expectedMsg + ' for the default route', () => {
     browser.get('');
-  });
 
-  it('should display: ' + expectedMsg, () => {
     expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
   });
 
