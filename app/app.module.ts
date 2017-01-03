0380e37e9a0fc055d51ef02970ce986ca8276a49
Axel Hodler <derxor@gmail.com>
2017-01-03 20:02:15 +0100

    add router functionality

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule}   from '@angular/forms';
-import {SubmitReportComponent}  from './reports/submit/submitreport.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {SubmitReportComponent} from "./reports/submit/submitreport.component";
 import {VerifyReport} from "./reports/verify/verifyreport.component";
 import {EthereumGateway} from "./boundaries/ethereumgateway";
 import {AppComponent} from "./app.component";
@@ -9,13 +9,21 @@ import {Config} from "./config";
 import {Contract} from "./boundaries/contract";
 import {ReportsGateway} from "./boundaries/reportsgateway";
 import {MockReportsGateway} from "./testdoubles/mockreportsgateway";
+import {RouterModule} from "@angular/router";
+import {ListReportsComponent} from "./reports/list/listreports.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'reports',
+        component: ListReportsComponent
+      }
+    ])
   ],
-  declarations: [AppComponent, SubmitReportComponent, VerifyReport],
+  declarations: [AppComponent, SubmitReportComponent, VerifyReport, ListReportsComponent],
   bootstrap: [AppComponent],
   providers: [EthereumGateway, Contract, Config, {provide: ReportsGateway, useClass: MockReportsGateway}]
 })
