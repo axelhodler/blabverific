4c160aab079213210774d3cbaa3ec45e4a5a05f8
Axel Hodler <derxor@gmail.com>
2017-01-03 11:21:31 +0100

    init display report details logic

--- app/reports/listreports.component.spec.ts
+++ app/reports/listreports.component.spec.ts
@@ -1,10 +1,11 @@
 import {ListReportsComponent} from "./listreports.component";
 import {ComponentFixture, async, TestBed} from "@angular/core/testing";
-import {By} from "@angular/platform-browser";
+import {ListReportsPageObject} from "./listreports.component.pageobject";
 
 describe('ListReportsComponent', () => {
   let comp: ListReportsComponent;
   let fixture: ComponentFixture<ListReportsComponent>;
+  let pageObject: ListReportsPageObject;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +16,7 @@ describe('ListReportsComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListReportsComponent);
+    pageObject = new ListReportsPageObject(fixture);
     comp = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -24,9 +26,17 @@ describe('ListReportsComponent', () => {
   });
 
   it('displays a list of reports', () => {
-    let reportsList = fixture.debugElement.query(By.css('#reports-list')).nativeElement;
+    let reportsList = pageObject.reportsList();
 
     expect(reportsList.tagName.toLowerCase()).toBe('ul');
     expect(reportsList.getElementsByTagName('li').length).toBe(2);
+  });
+
+  it('displays the reporters name, report contents and its id', () => {
+    let report = 'hashedreport report contents Sally the Submitter';
+    comp.reports = [report];
+    let reportsList = pageObject.reportsList();
+
+    expect(reportsList.firstElementChild.textContent).toBe(report)
   });
 });
