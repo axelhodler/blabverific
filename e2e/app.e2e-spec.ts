913f8283ef2c8c11fdea157dfe9f6fbe48cb41fa
Axel Hodler <derxor@gmail.com>
2017-01-03 21:01:23 +0100

    cover routing with e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -4,10 +4,15 @@ describe('Blabverific E2E Tests', () => {
 
   let expectedMsg = 'blockLAB Verify';
 
-  it('should display: ' + expectedMsg + ' for the default route', () => {
+  it('displays: ' + expectedMsg + ' for the default route', () => {
     browser.get('');
 
     expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
   });
 
+  it('can enter the reports route', () => {
+    browser.get('reports');
+
+    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  })
 });
