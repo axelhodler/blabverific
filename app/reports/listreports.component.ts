ba452928f2ac310d5e5d18a63c1677d3e72afd04
Axel Hodler <derxor@gmail.com>
2017-01-03 10:45:34 +0100

    extract template to file

--- app/reports/listreports.component.ts
+++ app/reports/listreports.component.ts
@@ -2,11 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   moduleId: module.id,
-  template:
-    `<ul id="reports-list">
-       <li>sample</li>
-       <li>sample</li>
-     </ul>`
+  templateUrl: 'listreports.component.html'
 })
 
 export class ListReportsComponent {
