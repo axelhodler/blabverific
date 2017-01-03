14b6c529869ef114eb8d8ca741313d00d6ee2137
Axel Hodler <derxor@gmail.com>
2017-01-03 20:42:51 +0100

    clean up after yourself

--- bin/run.sh
+++ bin/run.sh
@@ -11,10 +11,11 @@ sed -i '' 's#_CONTRACT_ADDRESS_#'$ADDRESS'#g' app/config.ts
 ABI=${CONFIG_CONTENTS_ARRAY[1]}
 sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' app/config.ts
 
-trap "git checkout app/config.ts" SIGINT
-
 if [ "$1" == "e2e" ]; then
-  tsc && concurrently "http-server -s" "./node_modules/protractor/bin/protractor protractor.config.js" --kill-others --success first && git checkout app/config.ts
+  tsc && concurrently "http-server -s" "./node_modules/protractor/bin/protractor protractor.config.js" --kill-others --success first
+  lsof -t -i:8545 | xargs kill
+  git checkout app/config.ts
 else
+  trap "git checkout app/config.ts" SIGINT
   npm run run
 fi
