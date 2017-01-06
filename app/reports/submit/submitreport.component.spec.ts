d579b7e8105613aa0e72a5b33e0671db83167737
Axel Hodler <derxor@gmail.com>
2017-01-06 11:30:20 +0100

    use the pageobject

--- app/reports/submit/submitreport.component.spec.ts
+++ app/reports/submit/submitreport.component.spec.ts
@@ -68,7 +68,7 @@ describe('SubmitReportComponent', () => {
   it('stores the report after submitting to the contract', fakeAsync(() => {
     pageObject.insertReportContent('my report');
 
-    comp.submitReport();
+    pageObject.clickSubmitReport();
     tick();
 
     expect(contractSpy.submitReport).toHaveBeenCalledWith('0xd2a1ba85429ae235e1572871497ae0d0e499c696cb44d33f88c2a26820e4f7cc');
