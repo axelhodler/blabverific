e8e5a997c32e8589eff042a74cb373c283e153aa
Axel Hodler <derxor@gmail.com>
2017-01-30 07:26:11 +0100

    grep was not finding anything due to t

--- bin/run.sh
+++ bin/run.sh
@@ -13,7 +13,7 @@ sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' app/config.ts
 
 if [ "$1" == "e2e" ]; then
   tsc && concurrently "lite-server -c configs/e2e-bs-config.js" "./node_modules/protractor/bin/protractor protractor.config.js" --kill-others --success first
-  lsof -t -i:8545 | grep node | xargs kill
+  lsof -i:8545 | grep node | awk '{print $2}' | xargs kill
   git checkout app/config.ts
 else
   trap "git checkout app/config.ts" SIGINT
