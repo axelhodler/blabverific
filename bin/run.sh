22f78ba91593fa45105770df50c95e493c55659f
Axel Hodler <derxor@gmail.com>
2017-02-27 07:48:47 +0100

    make sure docker is cleaned up

--- bin/run.sh
+++ bin/run.sh
@@ -13,7 +13,7 @@ sed -i '' 's#_CONTRACT_ABI_#'$ABI'#g' app/config.ts
 
 if [ "$1" == "e2e" ]; then
   tsc && concurrently "lite-server -c configs/e2e-bs-config.js" "./node_modules/protractor/bin/protractor protractor.config.js" --kill-others --success first
-  lsof -i:8545 | grep node | awk '{print $2}' | xargs kill
+  docker ps | tail -n 1 | awk '{print $1}' | xargs docker kill
   git checkout app/config.ts
 else
   trap "git checkout app/config.ts" SIGINT
